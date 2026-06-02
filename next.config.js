dce522c4f5e277ea0b0470b0c85cae974a7bf163
SimuImmo <ft.bu+feedback@ik.me>
2026-06-03 00:05:06 +0200

    disable swcMinify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    forceSwcTransforms: false,
-  },
+  swcMinify: false,
 }
 module.exports = nextConfig
